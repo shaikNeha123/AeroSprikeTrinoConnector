9e7e17bd9989c63021314432a1267226930cfbee
shaikNeha123 <Shaik.Neha@ibm.com>
2024-06-27 23:17:57 +0530

    Update AerospikeTransactionHandle.java

--- trinoAerospikeConnector/src/main/java/com/example/simpleaerospikeconnecter/src/main/java/com/example/aerospike/AerospikeTransactionHandle.java
+++ trinoAerospikeConnector/src/main/java/com/example/simpleaerospikeconnecter/src/main/java/com/example/aerospike/AerospikeTransactionHandle.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.aerospike;
+package com.example.simpleaerospikeconnecter.src.main.java.com.example.aerospike;
 
 import io.trino.spi.connector.ConnectorTransactionHandle;
 
